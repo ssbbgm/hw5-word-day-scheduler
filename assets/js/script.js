94745d40c4cd1bd7fb808cb4cba75667733e6bbb
Sherreka Burton <rekadarnb@gmail.com>
2022-03-31 14:40:21 -0400

    got coloring on calendar to work with data attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,51 @@
 var date = moment().format("dddd, MMM Do, YYYY");
 $("#currentDay").text(date);
+var currentTime = moment().hour();
 
-var currentTime = moment().format('h');
+$(document).ready(function() {
+
+    $(".saveBtn").on('click', function() {
+   
+    var time = $(this).siblings().attr('id');
+    var text = $(this).siblings('.description').val();
+
+
+    localStorage.setItem(time, text);
+
+    })
+
+    function timeKeeper (){
+        $(".time-block").each(function () {
+            var timeBlock = $(this).data("id");
+            if (timeBlock < currentTime){
+            $(this).removeClass('present')
+            $(this).removeClass('future')
+            $(this).addClass('past');
+            } else if (timeBlock === currentTime) {
+            $(this).removeClass('past')
+            $(this).removeClass('future')
+            $(this).addClass('present');
+            } else {
+            $(this).removeClass('present')
+            $(this).removeClass('past')
+            $(this).addClass('future');
+            }
+        })
+    }
+
+    
+
+    // Object.keys(localStorage).forEach(function(key){
+    //     console.log(localStorage.getItem(key));
+    //     let message = localStorage.getItem(key);
+
+    //     $('.description').textContent = message;
+
+    //  });
+
+
+
+
+    timeKeeper();
+
+})
