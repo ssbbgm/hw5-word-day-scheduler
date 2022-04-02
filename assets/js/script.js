41c69f3ae8da12d64a5444de8adb401db216fee2
Sherreka Burton <rekadarnb@gmail.com>
2022-04-01 20:17:10 -0400

    done with code

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,27 +34,10 @@ $(document).ready(function() {
         })
     }
 
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    for (let i=0; i < 8; i++) {
+        $(`#hour${i+9} .description`).val(localStorage.getItem(`hour${i+9}`));
+    }
 
     timeKeeper();
 
-
 })
-
-
-            // for (let i=0; i < localStorage.length; i++) {
-            //     $("#hour{i+9} .description").val(localStorage.getItem("hour9"));
-
-            // }
-        // $('div[id^="hour"] . document').each( function(){
-        //     var task = $('#description').val(localStorage.getItem('div[id^="hour"]'));
-        //     console.log(task);
-        // })
